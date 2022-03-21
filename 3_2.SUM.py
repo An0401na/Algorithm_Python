bce0daa9212f527a71155300bd4b48bd4b03ccb9
An Na <99172832+An0401na@users.noreply.github.com>
2022-03-21 20:52:03 +0900

    Create 3_2.SUM.py

---
+++ 3_2.SUM.py
@@ -0,0 +1,13 @@
+#배열의 원소들의 총합을 계산하는 recursive algorithm 설계하기
+
+def SUM(arr,start, end) :
+    if end-start ==1:
+        return arr[start]+arr[end]
+    else :
+        sum = SUM(arr, start,end-1)+arr[end]
+        return sum
+    
+arr=[5,5,1,2,0,7]
+sum = SUM(arr,0,arr.__len__()-1)
+print(sum)
+print(arr)
